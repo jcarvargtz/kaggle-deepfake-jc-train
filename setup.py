3a6c4949aebc0c44e792dcb09dbb4fef0625fce4
jcarvargtz <52109543+jcarvargtz@users.noreply.github.com>
2020-03-20 12:21:52 -0600

    si

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ REQUIRED_PACKAGES = ['docopt', 'pandas', 'keras', 'tensorflow','opencv-python','
 
 setup(
     name='Deepfake-challenge',
-    version='0.1',
+    version='0.2',
     author = 'Juan Carlos Vargas',
     author_email = 'jcarvargtz@hotmail.com',
     install_requires=REQUIRED_PACKAGES,
