5674c4b26eea24f844bc89a596a199927bd187ef
jcarvargtz <52109543+jcarvargtz@users.noreply.github.com>
2020-03-21 10:05:23 -0600

    1

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import find_packages
 from setuptools import setup
 
+
 REQUIRED_PACKAGES = ['docopt', 'pandas', 'keras', 'tensorflow','opencv-python','tqdm','pathlib','torch','torchvision']
 
 
 setup(
-    name='Deepfake-challenge',
+    name='trainer',
     version='0.5',
     author = 'Juan Carlos Vargas',
     author_email = 'jcarvargtz@hotmail.com',
     install_requires=REQUIRED_PACKAGES,
     packages=find_packages(),
-    package_data={'trainer':['blazeface.pth','anchors.npy']},
+    include_package_data=True,
+    package_data={'trainer':['*.pth','*.npy']},
     description="training setup, data download, and procesing for the kaggle's Deepfake detection challenge")
 
