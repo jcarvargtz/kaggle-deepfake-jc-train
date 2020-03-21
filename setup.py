c177b14f8403b1120e8f17e1e29109ff34830eb8
jcarvargtz <52109543+jcarvargtz@users.noreply.github.com>
2020-03-21 09:50:07 -0600

    creo que tal vez

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from setuptools import setup
 
 REQUIRED_PACKAGES = ['docopt', 'pandas', 'keras', 'tensorflow','opencv-python','tqdm','pathlib','torch','torchvision']
 
+
 setup(
     name='Deepfake-challenge',
-    version='0.4',
+    version='0.5',
     author = 'Juan Carlos Vargas',
     author_email = 'jcarvargtz@hotmail.com',
     install_requires=REQUIRED_PACKAGES,
     packages=find_packages(),
+    package_data={'trainer':['blazeface.pth','anchors.npy']},
     description="training setup, data download, and procesing for the kaggle's Deepfake detection challenge")
 
