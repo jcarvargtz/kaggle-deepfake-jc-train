2639226f1f0347c642537c2c5e0785553cd2da01
jcarvargtz <52109543+jcarvargtz@users.noreply.github.com>
2020-03-21 10:32:51 -0600

    porfa jala

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ REQUIRED_PACKAGES = ['docopt', 'pandas', 'keras', 'tensorflow','opencv-python','
 
 setup(
     name='trainer',
-    version='0.5',
+    version='0.6',
     author = 'Juan Carlos Vargas',
     author_email = 'jcarvargtz@hotmail.com',
     install_requires=REQUIRED_PACKAGES,
     packages=find_packages(),
     include_package_data=True,
-    package_data={'trainer':['*.pth','*.npy']},
+    package_data={'trainer':[r'trainer/*.pth',r'trainer/*.npy',r'trainer/*.sh']},
     description="training setup, data download, and procesing for the kaggle's Deepfake detection challenge")
 
